9643b604558bf0183d708ecba7cf4832f7111bbc
szalai1 <szalaipeti.vagyok@gmail.com>
2022-08-22 21:27:59 +0200

    freshen up

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # dotfiles
 
+# vim 
+`curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`
 `stow <APPNAME>`
+
+`bash ~/.macos`
 `launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist`
--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 for app in git macos tmux vim vscode zsh; do
     echo "Linking dotfile $app"
     stow $app
 done
 
 vim +'PlugInstall --sync' +qa
+bash ~/.macos
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
