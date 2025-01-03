839c25eeb7fb90de5dde86083155df86790e4d16
szalai1 <szalaipeti.vagyok@gmail.com>
2025-01-03 16:20:11 +0100

    Add log command

---
+++ raycast-scripts/log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title log
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "* " }
+
+# Documentation:
+# @raycast.description logthing to a file
+# @raycast.author szalai1
+# @raycast.authorURL https://raycast.com/szalai1
+
+d=`date +"%Y-%m-%d"` 
+t=`date +"%H:%M"`
+echo "- [[$d]] $t $1 \n" >>  ~/SynologyDrive/pages/RayCastLog.md
